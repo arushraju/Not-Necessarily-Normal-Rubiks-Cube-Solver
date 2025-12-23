e0bb5d1a46d75a688ba9f09331bbddb9b1856012
Arush Chinchkhede <125636290+arushraju@users.noreply.github.com>
2025-12-24 00:29:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -30,4 +30,5 @@ app.post('/solve', (req, res) => {
 });
 
 //Starts the server
-app.listen(3000, () => console.log("Server running on port 3000"));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
